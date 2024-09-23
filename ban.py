be88876d020786872b9c970a07996e3748005720
siv4kumar <21f3001256@ds.study.iitm.ac.in>
2024-09-23 17:21:18 +0530

    ban almost ready

--- ban.py
+++ ban.py
@@ -1,6 +1,14 @@
 from telegram import Update, ChatMemberUpdated
 from telegram.ext import Application ,ChatMemberHandler,CommandHandler, MessageHandler, CallbackContext,ContextTypes,filters
 from datetime import datetime,timedelta
+from db import User
+import asyncio
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy,session
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker,declarative_base
+
+
 
 
 # Replace 'YOUR_TOKEN' with your bot token from BotFather
@@ -9,8 +17,23 @@ TOKEN = '7334795697:AAEm1EATo2oxKU_vxCOcwqge91EsybSUalE'
 # Replace 'YOUR_CHAT_ID' with the ID of the group/channel you want to invite users to
 CHAT_ID = -1002338444521
 
-async def kickdaily(update:Update,context:CallbackContext):
-    await context.bot.kick_chat_member(chat_id=-1002338444521, user_id=1048272535)
-    print(f"Kicked ")
+DATABASE_URL = 'sqlite:///example.db'
+engine = create_engine(DATABASE_URL)
 
-kickdaily(Update,CallbackContext)
+
+async def users():
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for user in session.query(User).all():
+        print(user.userid)
+    print(f"********")
+
+    session.close()
+
+async def main():
+    await users()
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+    #await context.bot.ban_chat_member(chat_id=-1002338444521, user_id=1048272535)
